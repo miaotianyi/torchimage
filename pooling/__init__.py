a4422020df2e5bd3977049c9752f10f8c7ecd34e
miaotianyi <miaotianyi@cambricon.com>
2021-04-22 16:07:15 +0800

    Create __init__.py for pooling

--- pooling/__init__.py
+++ pooling/__init__.py
@@ -1,3 +1,8 @@
+"""
+We use pooling to refer to convolution operations that don't have learnable
+parameters, such as average pooling and gaussian pooling.
+"""
+
 from .gaussian import GaussianPool
 
 __all__ = [
