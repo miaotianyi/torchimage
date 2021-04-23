5a674a248fa0220e50ed1d2a45f7941b27c8e430
miaotianyi <miaotianyi@cambricon.com>
2021-04-23 13:05:21 +0800

    update docs and gitignore

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,6 +31,9 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx.ext.autodoc",  # automatic documentation from docstring
+    "sphinx.ext.napoleon",  # Numpy-style docstring
+    # "sphinx_rtd_theme"  # read the docs theme
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,7 +50,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
