a0580a51ad6a9b7a2927c5c205348e782cf0f234
miaotianyi <miaotianyi@cambricon.com>
2021-04-26 15:17:25 +0800

    update pooling documentation

--- torchimage/pooling/__init__.py
+++ torchimage/pooling/__init__.py
@@ -1,6 +1,17 @@
 """
-We use pooling to refer to convolution operations that don't have learnable
-parameters, such as average pooling and gaussian pooling.
+We use pooling to refer to convolution-like operations that don't have
+learnable parameters, such as average pooling and gaussian pooling.
+
+In torchimage, what makes pooling different from filtering is that
+a filter layer usually consists of its pooling layer counterpart
+with stride 1, after a "same" padding layer that ensures the output
+and the input will have the same shape. We separate filtering and pooling
+so that users can have greater freedom and the code becomes
+easier to maintain. Some may argue that in separable filters,
+padding each dimension before immediately convolving can save some
+computational costs, but we decide that copying a tensor for padding
+isn't worth it, considering that the padding elements are relatively
+few in comparison with the large amount of original tensor elements.
 """
 
 from .gaussian import GaussianPool
