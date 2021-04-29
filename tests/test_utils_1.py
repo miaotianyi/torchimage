13e75e7adc3506c960928d66774fd109c5c69561
miaotianyi <miaotianyi@cambricon.com>
2021-04-29 11:08:04 +0800

    update NdSpecs documentation

--- tests/test_utils_1.py
+++ tests/test_utils_1.py
@@ -4,12 +4,27 @@ from torchimage.utils import NdSpec
 
 class MyTestCase(unittest.TestCase):
     def test_1(self):
-        result = NdSpec([[1, 2]], item_shape=[2]).is_item
-        expected = False
+        obj = NdSpec([[1, 2]], item_shape=[2])
+        self.assertFalse(obj.is_item)
+        self.assertEqual(obj(-1), [1, 2])
 
-        self.assertEqual(
-            result, expected
-        )
+    def test_2(self):
+        obj = NdSpec([1, 2], item_shape=[2])
+        self.assertTrue(obj.is_item)
+        for i in range(1, 10):
+            self.assertEqual(obj(-i), [1, 2])
+
+    def test_3(self):
+        obj = NdSpec(423, item_shape=[])
+        self.assertTrue(obj.is_item)
+        for i in range(1, 10):
+            self.assertEqual(obj(-i), 423)
+
+    def test_4(self):
+        obj = NdSpec(423, item_shape=[2])
+        self.assertTrue(obj.is_item)
+        for i in range(1, 10):
+            self.assertEqual(obj(-i), [423, 423])
 
 
 if __name__ == '__main__':
--- torchimage/utils.py
+++ torchimage/utils.py
@@ -31,36 +31,79 @@ class NdSpec:
 
     Indeed, we assume that each item has the same shape, as no
     counterexample has been found in practice.
+    Feel free to raise an issue if you need such a functionality.
 
-    **NdSpec enforces a nested, right-justified, left-to-right storage format.**
+    **NdSpec enforces a nested, right-justified, left-to-right storage
+    format** when a list of items is passed.
+    Take padding width (number of padded elements before and after the
+    original tensor at a certain axis) for example, NdSpec format should be
+    ``((before_{axis}, after_{axis}), (before_{axis+1}, after_{axis+1}),
+    ..., (before_{n-2}, after_{n-2}), (before_{n-1}, after_{n-1}))``.
 
-    numpy pad is not right-justified: it requires every dimension to have item input, even if it's just (0, 0)
-    otherwise it will raise an error
+    - nested: Every element in the data tuple is an item.
+    - right-justified: The last item in data corresponds to the last axis
+      in the tensor of interest. Other axes are aligned correspondingly.
+    - left-to-right: The axis with smaller index comes first.
 
-    torch pad is not nested; it's also right-to-left
-
-
+    The motivation is best understood through comparison.
+    Numpy padding width format is
+    ``((before_0, after_0), (before_1, after_1), ..., (before_{n-1}, after_{n-1}))``.
+    PyTorch padding width format is
+    ``(before_{n-1}, after_{n-1}, before_{n-2}, after_{n-2}, ...,
+    before_{axis}, after_{axis})``,
+    such that axes less than ``axis`` are not padded.
+    Numpy padding width format is nested and left-to-right
+    (which makes it easier to read and index), but it forces users to
+    supply specification for every axis in an n-dimensional array.
+    This is cumbersome for (N, C, [D, ]H, W) data tensors, where
+    batch and channel dimensions usually don't need padding.
+    PyTorch padding width format is non-nested (ravel), right-to-left,
+    and right-justified. Ravel makes it harder to access padding width
+    for any specific axis without using pointer arithmetic. Right-to-left
+    is sometimes counterintuitive. It also doesn't support automatic
+    broadcasting (e.g. ``c -> [c, c] -> [[c, c]] * n`` where ``n``
+    is the number of padded dimensions). Therefore, torchimage
+    aims to get the best of both worlds with this new format.
     """
-    def __init__(self, data, item_shape):
+    def __init__(self, data, item_shape=()):
+        """
+        Parameters
+        ----------
+        data : scalar or array-like
+            A partial item, an item, or an iterable of items,
+            where each item is the specification for an axis.
+
+            A partial item (smaller than item_shape) will be
+            broadcast to item_shape, an item is kept as is,
+            and an iterable of items is stored in a nested,
+            right-justified, and left-to-right format.
+
+        item_shape : tuple
+            Expected shape of an item, i.e. ``np.array(item).shape``.
+        """
         self.item_shape = tuple(int(x) for x in item_shape)
         self.data = np.array(data)
 
         if self.data.shape == self.item_shape:
-            self.is_item = True  # data is item
+            self.n_items = None  # data is item
         elif self.data.ndim < len(self.item_shape):
             # requires extra broadcasting
             reps = list(self.item_shape)
-            reps[-self.data.ndim:] = [1] * self.data.ndim
+            if self.data.ndim > 0:
+                reps[-self.data.ndim:] = [1] * self.data.ndim
             self.data = np.tile(self.data, reps)
 
             assert self.data.shape == self.item_shape
-            self.is_item = True  # data is item now
+            self.n_items = None  # data is item now
         else:
             assert self.data.shape[1:] == self.item_shape
-            self.is_item = False
+            self.n_items = self.data.shape[0]
+
+    def is_item(self):
+        return self.n_items is None
 
     def __call__(self, axis: int, ndim=None):
-        if self.is_item:
+        if self.is_item():
             return self.data.tolist()
 
         if axis < 0:
@@ -71,6 +114,9 @@ class NdSpec:
 
         new_axis = axis - (ndim - self.data.shape[0])  # ignore leading axes (batch, channel, etc.)
         return self.data[new_axis].tolist()
+
+    def __repr__(self):
+        return f"NdSpec(data={self.data.tolist()}, item_shape={self.item_shape})"
 
     def format_numpy(self, ndim):
         pass
