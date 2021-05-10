39971a15607d34bfbfed75a385c137cfa110c7f4
miaotianyi <miaotianyi@cambricon.com>
2021-05-10 15:45:41 +0800

    finish unit tests for odd_symmetric and linear_ramp

--- tests/test_padder.py
+++ tests/test_padder.py
@@ -10,7 +10,7 @@ from torchimage.padding.generic_pad import GenericPadNd, _stat_padding_set
 class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
         self.tol = 1e-8
-        self.n_trials = 50
+        self.n_trials = 20
 
     def assertArrayEqual(self, a, b, tol=None, msg=None):
         if tol is None:
@@ -160,6 +160,55 @@ class MyTestCase(unittest.TestCase):
                         actual = GenericPadNd(pad_width=pw, mode=mode_ti)(arr_2).numpy()
                         print("Done.")
                         self.assertArrayEqual(expected, actual)
+
+    def test_odd_symmetric(self):
+        for _ in range(self.n_trials):
+            ndim = np.random.randint(1, 6)
+            shape = np.random.randint(1, 7, ndim)
+
+            # Note: cannot exceed original length
+            # otherwise numpy padding runs into a known bug
+            pad_width_scalar = np.random.randint(1, min(shape) + 1)
+            pad_width_pair = np.random.randint(1, min(shape) + 1, 2).tolist()
+            pad_width_list = np.random.randint(1, np.tile((shape + 1).reshape(-1, 1), [1, 2])).tolist()
+
+            arr_1 = np.random.rand(*shape)
+            arr_2 = torch.from_numpy(arr_1)
+
+            for pw in [pad_width_scalar, pad_width_pair, pad_width_list]:
+                with self.subTest(shape=shape, pad_width=pw):
+                    print(f"{shape=}, {pw=}")
+                    print("calculating expected")
+                    expected = np.pad(arr_1, pad_width=pw, mode="symmetric", reflect_type="odd")
+                    print("calculating actual")
+                    actual = GenericPadNd(pad_width=pw, mode="odd_symmetric")(arr_2).numpy()
+                    print("Done.")
+                    self.assertArrayEqual(expected, actual)
+
+    def test_linear_ramp(self):
+        mode = "linear_ramp"
+        for _ in range(self.n_trials):
+            ndim = np.random.randint(1, 6)
+            shape = np.random.randint(1, 7, ndim)
+
+            # fine for pad width to exceed original length
+            pad_width_scalar = np.random.randint(0, 10)
+            pad_width_pair = np.random.randint(0, 10, 2).tolist()
+            pad_width_list = np.random.randint(0, 10, (ndim, 2)).tolist()
+
+            end_values_scalar = np.random.rand()
+            end_values_pair = np.random.rand(2).tolist()
+            end_values_list = np.random.rand(ndim, 2).tolist()
+
+            arr_1 = np.random.rand(*shape)
+            arr_2 = torch.from_numpy(arr_1)
+
+            for pw, ev in product([pad_width_scalar, pad_width_pair, pad_width_list],
+                                  [end_values_scalar, end_values_pair, end_values_list]):
+                with self.subTest(shape=shape, pad_width=pw, end_values=ev):
+                    expected = np.pad(arr_1, pad_width=pw, mode=mode, end_values=ev)
+                    actual = GenericPadNd(pad_width=pw, mode=mode, end_values=ev)(arr_2).numpy()
+                    self.assertArrayEqual(expected, actual)
 
 
 if __name__ == '__main__':
