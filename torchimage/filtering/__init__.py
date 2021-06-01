b22e398fc95ab7551e5389c2e47e75599c122b92
Tianyi Miao <tennielmiao@gmail.com>
2021-06-01 20:01:39 +0800

    add unsharp mask

--- torchimage/filtering/__init__.py
+++ torchimage/filtering/__init__.py
@@ -1,6 +1,6 @@
 from .decorator import pool_to_filter
 from .edges import EdgeDetector, Sobel, Prewitt, Farid, Scharr, GaussianGrad
-
+from .sharpen import UnsharpMask
 
 from .base import SeparableFilterNd
 from .generic import GenericFilter2d
@@ -8,6 +8,7 @@ from .generic import GenericFilter2d
 __all__ = [
     "pool_to_filter",
     "EdgeDetector", "Sobel", "Prewitt", "Farid", "Scharr", "GaussianGrad",
+    "UnsharpMask",
 
     "SeparableFilterNd",
     "GenericFilter2d"
