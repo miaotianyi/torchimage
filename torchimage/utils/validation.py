aeced2780a9449503a72f6c044776458f1409d94
Tianyi Miao <tennielmiao@gmail.com>
2021-06-04 17:41:47 +0800

    add more validation tools

--- torchimage/utils/validation.py
+++ torchimage/utils/validation.py
@@ -32,3 +32,27 @@ def check_axes(x, axes):
         axes = tuple(int(a) if a >= 0 else x.ndim + int(a) for a in axes)
         assert all(0 <= a <= x.ndim for a in axes)
     return axes
+
+
+def check_stride(stride):
+    try:
+        stride = int(stride)
+    except TypeError:
+        raise ValueError(f"stride must be integer, got {stride} of type {type(stride)} instead")
+
+    if stride < 1:
+        raise ValueError(f"stride must be positive, got {stride} instead")
+    return stride
+
+
+def check_pad_width(pad_before, pad_after):
+    try:
+        pad_before, pad_after = int(pad_before), int(pad_after)
+    except TypeError:
+        raise ValueError(f"padding width must be integer, got {pad_before} and {pad_after} instead")
+
+    if pad_before < 0 or pad_after < 0:
+        raise ValueError(f"padding width must be nonnegative, got {pad_before} and {pad_after} instead")
+    return pad_before, pad_after
+
+
