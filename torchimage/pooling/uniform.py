99eb132e080acedbdc1066984e6389888886025b
Tianyi Miao <tennielmiao@gmail.com>
2021-06-17 14:59:57 +0800

    Faster average pooling nd

--- torchimage/pooling/uniform.py
+++ torchimage/pooling/uniform.py
@@ -1,47 +1,40 @@
 import numpy as np
 import torch
 
-from .base import BasePoolNd, SeparablePoolNd
+from .base import SeparablePoolNd
 
-from ..padding import Padder
 from ..utils import NdSpec
 from ..utils.validation import check_axes
 from ..shapes.conv_like import n_original_elements_nd
-from .base import move_tensor
 
 
-class AvgPoolNd(BasePoolNd):  # (nn.Module):
+class AvgPoolNd(SeparablePoolNd):
     @staticmethod
     def _get_kernel(kernel_size):
-        # this method is useful if inheriting SeparablePoolNd is actually faster
         return NdSpec(kernel_size).map(lambda ks: (np.ones(ks) / ks).tolist())
 
-    def __init__(self, kernel_size, stride=None, *, count_include_pad=True,
-                 same_padder=None):
-        super().__init__(same_padder=same_padder)
-
-        self.kernel_size = NdSpec(kernel_size, item_shape=[])
-        self.stride = self.read_stride(stride)
+    def __init__(self, kernel_size, stride=None, *, count_include_pad=True, same_padder=None):
+        super().__init__(
+            kernel=self._get_kernel(kernel_size=kernel_size),
+            stride=stride,
+            same_padder=same_padder
+        )
         self.count_include_pad = count_include_pad
-        self._align_params()
-
-    def _align_params(self):
-        self.ndim = NdSpec.agg_index_len(self.kernel_size, self.stride, allowed_index_ndim=(0, 1))
 
     def forward(self, x: torch.Tensor, axes=slice(2, None)):
-        axes = check_axes(x, axes)
-        assert self.ndim == 0 or self.ndim == len(axes)
-
-        input_shape = x.shape
-
-        # initialize same padder
-        x = self.pad(x, axes=axes)
-
         if self.count_include_pad:
-            z = np.prod([self.kernel_size[i] for i in range(len(axes))])
+            return super().forward(x, axes=axes)
+
         else:
+            input_shape = x.shape
+            axes = check_axes(x, axes)
+
+            z_old = np.prod([self.kernel_size[i] for i in range(len(axes))])
+
             idx = np.argsort(axes)  # axes must be sorted
             assert len(axes) == len(set(axes))  # axes must be unique
+
+            x = super().forward(x, axes=axes)
             z = n_original_elements_nd(
                 in_size=[input_shape[axes[i]] for i in idx],
                 pad_width=[self.same_padder.pad_width[i] for i in idx],
@@ -54,12 +47,5 @@ class AvgPoolNd(BasePoolNd):  # (nn.Module):
                 new_shape[axes[i]] = z.shape[i]
             z = z.view(*new_shape)
 
-        for i, axis in enumerate(axes):
-            if self.kernel_size[i] <= 1:
-                continue
-
-            x = x.unfold(axis, size=self.kernel_size[i], step=self.stride[i]).sum(dim=-1, keepdim=False)
-
-        x = x / z
-
-        return x
+            x = x * z_old / z
+            return x
