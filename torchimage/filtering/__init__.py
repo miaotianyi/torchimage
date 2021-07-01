75f6684b23bc11f0e89c0a0e4f21b3ebff4b18ac
Tianyi Miao <tennielmiao@gmail.com>
2021-07-01 15:29:28 +0800

    remove inefficient generic filter

--- torchimage/filtering/__init__.py
+++ torchimage/filtering/__init__.py
@@ -1,12 +1,8 @@
 from .edges import EdgeFilter, Sobel, Prewitt, Farid, Scharr, GaussianGrad, Laplace, LaplacianOfGaussian
 from .sharpen import UnsharpMask
 
-from .generic import GenericFilter2d
-
 __all__ = [
     "EdgeFilter", "Sobel", "Prewitt", "Farid", "Scharr", "GaussianGrad",
     "Laplace", "LaplacianOfGaussian",
     "UnsharpMask",
-
-    "GenericFilter2d"
 ]
