87456d8cdc8fa9fd1b50fe0193bfb46cd56e71e6
Tianyi Miao <tennielmiao@gmail.com>
2021-07-02 16:58:40 +0800

    fix backward compatibility of math prod

--- torchimage/pooling/operator.py
+++ torchimage/pooling/operator.py
@@ -1,4 +1,4 @@
-from math import prod
+import numpy as np
 import torch
 from torch.nn import functional as F
 
@@ -35,7 +35,7 @@ def any_conv_1d(x: torch.Tensor, w: torch.Tensor, *, dim: int, stride: int, dila
         w = w.view(1, 1, -1)  # out_channels, in_channels/groups, input kernel width
         x = F.conv1d(x, w, bias=None, stride=stride, padding=0, dilation=dilation, groups=1)
     else:
-        x = x.view(prod(x.shape[:dim]), 1, x.shape[dim], -1)
+        x = x.view(np.prod(x.shape[:dim]), 1, x.shape[dim], -1)
         w = w.view(1, 1, -1, 1)  # out_channels, in_channels/groups, input kernel height, input kernel width
         x = F.conv2d(x, w, bias=None, stride=(stride, 1), padding=0, dilation=(dilation, 1), groups=1)
 
